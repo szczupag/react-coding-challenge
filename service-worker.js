84b2399ee1ed2e590215bd1a7e52c6c7779b0903
Agata Szczuka <agata.szczuka@gmail.com>
2020-10-06 19:14:00 +0200

    Create and register service worker to cache fetch responses.

---
+++ service-worker.js
@@ -0,0 +1,9 @@
+var CACHE_NAME = 'fetch-requests-cache';
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+      caches.match(event.request).then(function(response) {
+          return response || fetch(event.request);
+      })
+  );
+});
